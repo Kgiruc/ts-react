d0d8ed6c22888d08afcfb9050e7eb80de7e1c41a
Kgiruc <kgiruc76@gmail.com>
2023-04-11 14:06:32 +0200

    tsx basic

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,14 +1,15 @@
-import Button from "./componenets/Button"
-import Input from "./componenets/Input"
+import Button from "./componenets/Button";
+import Container from "./componenets/Container";
+import Input from "./componenets/Input";
 
 function App() {
-  
   return (
     <div className="App">
-      <Button  handleClick={(event, id) => console.log('button', event, id)}/>
-      <Input value="" handleChange={event => console.log(event)}/>
+      <Button handleClick={(event, id) => console.log("button", event, id)} />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid green", padding: "1rem" }} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ts-react/src/componenets/Container.tsx
@@ -0,0 +1,13 @@
+type ContainerProps = {
+    styles: React.CSSProperties
+}
+
+function Container(props: ContainerProps) {
+  return (
+    <div >
+        Container
+    </div>
+  );
+}
+
+export default Container;
