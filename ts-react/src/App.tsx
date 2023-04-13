7c76458ea69012f197235a6919bfe302224b3b40
Kgiruc <kgiruc76@gmail.com>
2023-04-13 13:10:26 +0200

    tsx basic

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,16 +1,13 @@
-import Button from "./componenets/Button";
-import Container from "./componenets/Container";
-import Input from "./componenets/Input";
-import Counter from "./componenets/state/Counter";
-import LoggedIn from "./componenets/state/LoggedIn";
-import User from "./componenets/state/User";
+import Box from "./componenets/context/Box";
+import ThemeContextProvider from "./componenets/context/ThemeContext";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <LoggedIn /> */}
-      {/* <User /> */}
-      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
