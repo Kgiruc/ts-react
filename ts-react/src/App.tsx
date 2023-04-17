c8a036588a94446ecf50c5cb12072706ce5567bb
Kgiruc <kgiruc76@gmail.com>
2023-04-17 13:13:56 +0200

    tsx basic

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -4,12 +4,29 @@ import DomRef from "./componenets/ref/DomRef";
 import MutableRef from "./componenets/ref/MutableRef";
 import Profile from "./componenets/auth/Profile";
 import Private from "./componenets/auth/Private";
+import List from "./componenets/generics/List";
+import RandomNumber from "./componenets/restriction/RandomNumber";
 
+
+const listelo = [
+  {
+    first: 'elo',
+    last: 'zero'
+  }, 
+  {
+    first: 'belo',
+    last: 'fero'
+  }, 
+  {
+    first: 'helo',
+    last: 'perospero'
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile}/>
+    <RandomNumber value={12} isPositive isNegative isZero/>
     </div>
   );
 }
